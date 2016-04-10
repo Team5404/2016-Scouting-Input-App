2d20cc62537411988b21df6c914f21bee33652b8
michchinn <michchinn@users.noreply.github.com>
2016-04-09 22:17:21 -0400

    Update MatchRecord

---
+++ MatchRecord.java
@@ -0,0 +1,13 @@
+/*
+This file defines a match record.
+It contains all of the fields that describe how a team performed in a given match.
+*/
+
+public class MatchRecord {
+
+  private int teamNumber;
+  private int matchNumber;
+
+
+
+}
